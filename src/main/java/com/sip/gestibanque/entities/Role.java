667f91af3167619d00963879f8f27d7d7e4c1ce4
moahmed amine mezghich <git@mezghich.com>
2024-01-26 16:44:03 +0100

    relation ManyToMany

---
+++ src/main/java/com/sip/gestibanque/entities/Role.java
@@ -0,0 +1,49 @@
+package com.sip.gestibanque.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String libelle;
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", libelle=" + libelle + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public Role(int id, String libelle) {
+		super();
+		this.id = id;
+		this.libelle = libelle;
+	}
+	
+	public Role() {
+	
+	}
+	
+
+}
--- src/main/java/com/sip/gestibanque/entities/User.java
+++ src/main/java/com/sip/gestibanque/entities/User.java
@@ -1,10 +1,15 @@
 package com.sip.gestibanque.entities;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 @Entity
 public class User {
 	
@@ -18,6 +23,20 @@ public class User {
 	private String adresse;
 	private String telephone;
 	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+
 	public User() {
 		super();
 	}
